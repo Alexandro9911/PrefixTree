53ec9df3f19476e77edb43e6fabb89ac3a41958c
Alexandro9911 <31696942+Alexandro9911@users.noreply.github.com>
2018-03-24 17:34:46 +0300

    comments fix

--- src/Node.java
+++ src/Node.java
@@ -4,29 +4,80 @@ import java.util.*;
 
 public class Node {
 
-    HashMap<Character, Node> child = new HashMap<>();
+    TreeMap<Character, Node> children = new TreeMap<>();
+
+
+    /**
+     * @return Collection of children
+     */
 
     public Collection<Node> childNodes() {
-        return child.values();
+        return children.values();
     }
+
+    /**
+     * @param c char
+     * @return make Node with char c as a current Node
+     */
 
     Node getch(char c) {
-        return child.get(c);
+        return children.get(c);
     }
+
+    /**
+     * @return quantity of children
+     */
 
     public int sizeof() {
-        if (child.size() == 0) {
+        if (children.size() == 0) {
             return 0;
         }
-        return child.size();
+        return children.size();
     }
 
+    /**
+     * @param c char
+     * @return successful or not
+     */
+
     boolean deleteLastNode(char c) {
-        if (child.get(c).sizeof() == 0) {
-            child.remove(c);
+        if (children.get(c).sizeof() == 0) {
+            children.remove(c);
             return true;
         } else
             return false;
     }
 
+    /**
+     * @param ch char
+     * @return all strings with this char
+     */
+
+    public List<String> findAll(char ch) {
+        List<String> answ = new ArrayList<>();
+        String str = "";
+        Node currNode = new Node();
+        currNode = this.getch(ch);
+        str += ch;
+        Object[] keyArr = currNode.children.keySet().toArray();
+        Object[] keyInt = currNode.children.keySet().toArray();
+        int Q = keyArr.length;
+        if (Q == 0) {
+            answ.add(str);
+            return answ;
+        } else {
+            for (int i = 0; i != Q; i++) {
+                char[] next = keyArr[i].toString().toCharArray();
+                currNode = this.getch(next[0]);
+                keyInt = this.children.keySet().toArray();
+                int N = keyInt.length;
+                if (N == 0) {
+                    str += next[i];
+                } else {
+                    return findAll(next[0]);
+                }
+            }
+            return answ;
+        }
+    }
 }
--- src/PrefixTrie.java
+++ src/PrefixTrie.java
@@ -10,31 +10,19 @@ public final class PrefixTrie {
      * @param str it is a string in tree
      * @return list of nodes of each char of parameter str
      */
-    public ArrayList<Node> listNodes(String str) {
+    public List<Node> listNodes(String str) {
         Node point = root;
-        ArrayList<Node> nodes = new ArrayList<>();
+        List<Node> nodes = new ArrayList<>();
         if (str.length() == 0) throw new IllegalArgumentException();
         for (char part : str.toLowerCase().toCharArray()) {
-            if (!point.child.containsKey(part)) {
+            if (!point.children.containsKey(part)) {
                 break;
             } else {
-                point = point.child.get(part);
+                point = point.children.get(part);
                 nodes.add(point);
             }
         }
         return nodes;
-    }
-
-    /**
-     * @param node -- node
-     * @return has this node children or not
-     */
-    public boolean hasChild(Node node) {
-        Node point = node;
-        boolean has;
-        if (point.child.size() == 0) has = false;
-        else has = true;
-        return has;
     }
 
     /**
@@ -43,11 +31,14 @@ public final class PrefixTrie {
      */
     public boolean input(String str) {
         Node point = root;
+        if (str.toCharArray().length == 0 || PrefixTrie.this.find(str)) {
+            return false;
+        }
         for (Character PartKey : str.toLowerCase().toCharArray()) {
-            if (!point.child.containsKey(PartKey)) {
-                point.child.put(PartKey, new Node());
+            if (!point.children.containsKey(PartKey)) {
+                point.children.put(PartKey, new Node());
             }
-            point = point.child.get(PartKey);
+            point = point.children.get(PartKey);
         }
         return true;
     }
@@ -58,57 +49,56 @@ public final class PrefixTrie {
      */
     public boolean find(String str) {
         Node point = root;
+        if (str.length() == 0) {
+            return false;
+        }
         for (char part : str.toLowerCase().toCharArray()) {
-            if (!point.child.containsKey(part)) {
+            if (!point.children.containsKey(part)) {
                 return false;
             } else {
-                point = point.child.get(part);
+                point = point.children.get(part);
             }
         }
         return true;
     }
 
     /**
-     * @param s string which you want to delete
+     * @param str string which you want to delete
      * @return success or not
      */
 
-    public boolean delete(String s) {
-        Deque<Node> stack = new LinkedList<>();
-        char[] straight = s.toCharArray();
-        Node finder = root;
-        for (char c : straight)
-            if (finder.getch(c) != null) {
-                stack.addFirst(finder);
-                finder = finder.getch(c);
-            } else return false;
-        StringBuilder builder = new StringBuilder(s);
-        builder = builder.reverse();
-        char[] reverse = builder.toString().toCharArray();
-        for (char c : reverse) {
-            Node del = stack.removeFirst();
-            if (!del.deleteLastNode(c))
-                return true;
+    public boolean delete(String str) {
+        boolean check = PrefixTrie.this.find(str);
+        Node point = root;
+        if (!check) {
+            return false;
+        } else {
+            List<Node> nodes = listNodes(str);
+            for (int m = nodes.size() - 1; m > 0; m--) {
+                point = nodes.get(m);
+                if (point.childNodes().size() == 0) {
+                    point.children.clear();
+                } else {
+                    Node childNode = nodes.get(m);
+                    point.children.replace(point.children.firstKey(), childNode);
+                }
+            }
+            return true;
         }
-        return true;
     }
 
     /**
      * @param str prefix
      * @return all strings which starts from prefix "str"
      */
-    public List<String> findAll(String str) {
+    public List<String> findAllStrings(String str) {
         char[] prefix = str.toCharArray();
         Node currNode = root;
         boolean check = PrefixTrie.this.find(str);
         List<String> answ = new ArrayList<>();
-        String firstPart = new String();
-        String partStr = new String();
-        String finalString = new String();
-
-        Object[] keyArr = currNode.child.keySet().toArray();
-        Object[] nodArr = currNode.childNodes().toArray();
-
+        String firstPart = "";
+        String partStr = "";
+        String finalString = "";
         if (!check) throw new IllegalArgumentException();
         else {
             for (char ch : prefix) {
@@ -116,65 +106,16 @@ public final class PrefixTrie {
                     break;
                 }
                 firstPart = firstPart + ch;
-                currNode = currNode.child.get(ch);
-
-                if (!hasChild(currNode)) {
+                currNode = currNode.children.get(ch);
+                if (currNode.childNodes().size() == 0) {
                     finalString = firstPart + partStr;
                     answ.add(finalString);
                     return answ;
                 }
+                currNode.findAll(prefix[prefix.length - 1]);
             }
-            while (hasChild(currNode)) {
-                while (hasChild(currNode) && currNode.childNodes().size() == 1) {
-                    Object[] part = currNode.child.keySet().toArray();
-                    char[] next = part[0].toString().toCharArray();
-                    currNode = currNode.getch(next[0]);
-                    partStr += part[0];
-                }
-                finalString = firstPart + partStr;
-                answ.add(finalString);
-            }
-        }
-        return answ;
-    }
-
-    /**
-     * @param ch char
-     * @return List of all possible strings, which starts from char ch
-     */
-
-    public List<String> findAllStrings(char ch) {  // НЕ РОБИТ
-        List<String> answ = new ArrayList<>();
-        String finalString = new String();
-        String partString = new String();
-        String firstString = new String();
-        Object[] keyArr;
-        firstString += ch;
-
-        Node currNode = root.getch(ch);
-
-        keyArr = currNode.child.keySet().toArray();
-
-        if (keyArr.length == 0) {
-            System.out.println("last"); //  далее выводы на консоль для проверки правильности кода
-            finalString = firstString + partString;
+            finalString = firstPart + partStr;
             answ.add(finalString);
-            return answ;
-        } else {
-            System.out.println("length Arr = " + keyArr.length);
-            for (int i = 0; i < keyArr.length; i++) {
-                System.out.println("loop");
-                partString += keyArr[i];
-                System.out.println("currString = " + firstString + partString);
-                char[] c = partString.toCharArray();
-                System.out.println("c[i]= " + c[i]);
-                System.out.println("i=" + i);
-                currNode = currNode.getch(c[i]);
-                keyArr = currNode.child.keySet().toArray();
-                System.out.println("length Arr = " + keyArr.length);
-                answ = findAllStrings(c[i]);
-                return answ;
-            }
         }
         return answ;
     }
