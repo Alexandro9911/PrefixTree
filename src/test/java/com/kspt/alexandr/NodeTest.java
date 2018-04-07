17c6a6df16b9c13ea40b2619ce16f524975f1e08
Alexandro9911 <31696942+Alexandro9911@users.noreply.github.com>
2018-04-07 14:35:32 +0300

    comments fix

---
+++ src/test/java/com/kspt/alexandr/NodeTest.java
@@ -0,0 +1,46 @@
+package com.kspt.alexandr;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public final class NodeTest {
+    @Test
+    public void deleteNode() {
+        PrefixTrie a = new PrefixTrie();
+        Node currNode = a.root;
+        char[] key = "qwerty".toCharArray();
+        a.input("abcd");
+        a.input("q");
+        assertEquals(true, currNode.deleteNode(key[0]));
+    }
+
+    @Test
+    public void listNodes() {
+        PrefixTrie a = new PrefixTrie();
+        String str = "abcd";
+        int stringLength = str.length();
+        a.input(str);
+        Deque<Node> answ = a.listNodes(str);
+        assertEquals(stringLength, answ.size());
+    }
+
+    @Test
+    public void findAll() {
+        PrefixTrie a = new PrefixTrie();
+        a.input("abcd");
+        a.input("aqwe");
+        a.input("asdf");
+        a.input("answ");
+        List<String> answ = new ArrayList<>();
+        answ.add("aqwe");
+        answ.add("abcd");
+        answ.add("asdf");
+        answ.add("answ");
+        assertEquals(answ, a.root.getAllWords());
+    }
+}
